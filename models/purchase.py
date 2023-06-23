82561e69532769b1ef1efff092376b0b3c1c3c41
Sébastien Bühl <buhlsebastien@gmail.com>
2023-06-23 14:50:08 +0200

    [IMP] Add condition if BL/BR cancelled
    Check if the quantity ordered = quantity delivered

--- models/purchase.py
+++ models/purchase.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
+from odoo.tools import float_compare
 
 
 class PurchaseOrder(models.Model):
@@ -18,7 +19,10 @@ class PurchaseOrder(models.Model):
             if not order.picking_ids or all(p.state == 'cancel' for p in order.picking_ids):
                 order.receipt_status = False
             elif all(p.state in ['done', 'cancel'] for p in order.picking_ids):
-                order.receipt_status = 'full'
+                if all(float_compare(line.qty_received, line.product_qty, precision_rounding=line.product_uom.rounding) >= 0 for line in order.order_line):
+                    order.receipt_status = 'full'
+                else:
+                    order.receipt_status = 'partial'
             elif any(p.state == 'done' for p in order.picking_ids):
                 order.receipt_status = 'partial'
             else:
